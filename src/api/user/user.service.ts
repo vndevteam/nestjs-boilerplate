05e3c6df8e0b5e4e42541785ed9f38e8bc6d4583
Lâm Ngọc Khương <me@ngockhuong.com>
2024-06-07 21:31:16 +0700

    feat(eslint): add eslint v9 (#1)

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  create(createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
