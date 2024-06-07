7ec0125ceddba4d919bac159d9ee4698ba7a5065
Lâm Ngọc Khương <me@ngockhuong.com>
2024-06-07 22:40:20 +0700

    feat: add husky, commitlint, lint-staged (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 
 @Module({
   imports: [ApiModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
