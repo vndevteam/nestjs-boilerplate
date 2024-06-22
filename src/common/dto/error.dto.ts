fadb436b038494ceef7a1fae191b2b7ccc98af54
Lam Ngoc Khuong <me@ngockhuong.com>
2024-06-22 16:41:16 +0700

    feat: fix cmt

--- src/common/dto/error.dto.ts
+++ src/common/dto/error.dto.ts
@@ -1,11 +1,25 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ErrorDetailDto } from './error-detail.dto';
 
 export class ErrorDto {
+  @ApiProperty()
   timestamp: string;
+
+  @ApiProperty()
   statusCode: number;
+
+  @ApiProperty()
   error: string;
+
+  @ApiProperty()
   message: string;
+
+  @ApiPropertyOptional({ type: ErrorDetailDto, isArray: true })
   details?: ErrorDetailDto[];
+
+  @ApiPropertyOptional()
   stack?: string;
+
+  @ApiPropertyOptional({ type: Error })
   trace?: Error | unknown;
 }
