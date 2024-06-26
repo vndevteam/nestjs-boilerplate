cf833748db88c1b7e7cf2f565f192d63dbb3c618
Lam Ngoc Khuong <me@ngockhuong.com>
2024-06-26 23:04:06 +0700

    feat: add helmet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,11 @@ export default tsEslint.config(
         sourceType: 'module',
       },
     },
-    ignores: ['eslint.config.mjs', 'docs/.vuepress/**/*', 'i18n.generated.ts'],
+    ignores: [
+      'eslint.config.mjs',
+      'docs/.vuepress/**/*',
+      'src/generated/i18n.generated.ts',
+    ],
     rules: {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
