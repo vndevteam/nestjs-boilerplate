f83c97456628930533ba294f338a34caa9fc23d6
Lam Ngoc Khuong <me@ngockhuong.com>
2024-07-08 00:02:26 +0700

    feat: add url env and home module

--- src/utils/validate-config.ts
+++ src/utils/validate-config.ts
@@ -13,7 +13,18 @@ function validateConfig<T extends object>(
   });
 
   if (errors.length > 0) {
-    throw new Error(errors.toString());
+    const errorMsg = errors
+      .map(
+        (error) =>
+          `\nError in ${error.property}: \n` +
+          Object.entries(error.constraints)
+            .map(([key, value]) => `+ ${key}: ${value}`)
+            .join('\n'),
+      )
+      .join('\n');
+
+    console.error(`\n${errors.toString()}`);
+    throw new Error(errorMsg);
   }
   return validatedConfig;
 }
