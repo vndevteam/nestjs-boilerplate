ed8efe3d76b5005577ad9b49a74d907006332512
Lam Ngoc Khuong <me@ngockhuong.com>
2024-08-29 01:22:50 +0700

    feat: fix cors and add unit test

--- setup-jest.mjs
+++ setup-jest.mjs
@@ -1,2 +1,11 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+
 process.env.NODE_ENV = 'test';
+
+global.console = {
+  ...global.console,
+  log: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
